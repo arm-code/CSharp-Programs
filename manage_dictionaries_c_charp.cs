142011fd759d45a772e3ceb695100263cb90063d
Alexis Romero Mendoza <64325158+arm-code@users.noreply.github.com>
2022-05-18 11:39:48 -0600

    Update manage_dictionaries_c_charp.cs

--- manage_dictionaries_c_charp.cs
+++ manage_dictionaries_c_charp.cs
@@ -1,7 +1,7 @@
 /*
 * THIS FILE MANAGE A DICTIONARY IN C#
 * READ DATASET, SAVE INTO DICTIORNARY AND THEN SEARCH SOME RANDOM VALUE NAME.
-* 
+* THIS PROGRAM WORK WITH 1000 VALUES
 */
 using System;
 using System.Collections.Generic;
@@ -17,6 +17,20 @@ class Solution {
             string [] words = value.Split(' ');
                 dic.Add(words[0], words[1]);            
         }  
+        /*
+        *THIS BLOCK CODE ONLY WORK WITH VALUES < 1000, THEN, SHOW A RUNTIME ERROR
+        for(int j = 0; j < n; j++){
+            string search = Console.ReadLine();            
+            if(dic.ContainsKey(search)){                   
+                Console.WriteLine(search + "=" + dic[search]);
+            }else{
+                Console.WriteLine("Not found");
+            }
+        }
+        
+        */
+        
+        /* HERE IT WORK WITH 1000 VALUES, OTHERS IMPLEMETATION DOES NOT WORK */
         String name;        
         while (( name = Console.ReadLine()) != null)
         {
